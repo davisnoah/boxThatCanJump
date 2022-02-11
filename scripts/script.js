30c3aefdee0cd7454132eb5b0425c4f05b60bcb2
Noah Davis <97123176+davisnoah8079@users.noreply.github.com>
2022-02-11 08:35:16 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,12 @@
 const c = document.querySelector("#c");
 const ctx = c.getContext("2d");
 
-//Defines a standard unit of measurement to make game responsive
+//Defines width and height of canvas
 c.width = window.innerWidth * 4 / 5;
 c.height = c.width / 2;
-const unit = c.width/100;
+
+//Defines a standard unit of measurement to make game responsive
+const unit = c.width / 100;
 
 
 /* ===============================
@@ -14,14 +16,14 @@ const unit = c.width/100;
 
 //Constructs GameComp Class
 class GameComponent {
-  constructor (type) {
+  constructor(type) {
     this.type = type;
   }
 }
 
 //Constructs Physics Class
 class Physics extends GameComponent {
-  constructor (magnitude) {
+  constructor(magnitude) {
     super(["physics"]);
     this.magnitude = magnitude;
   }
@@ -29,15 +31,15 @@ class Physics extends GameComponent {
 
 //Constructs Entity Class
 class Structure extends GameComponent {
-  constructor (type, width, height, x, y, color) {
+  constructor(type, width, height, x, y, color) {
     super(type)
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
     this.color = color;
-    this.xCenter = this.x + (this.width/2);
-    this.yCenter = this.y + (this.height/2);
+    this.xCenter = this.x + (this.width / 2);
+    this.yCenter = this.y + (this.height / 2);
   }
 
   //Draws a Structure
@@ -49,41 +51,41 @@ class Structure extends GameComponent {
 
   //Updates the coords of the center of the structure with new coords
   updateCenter() {
-    this.xCenter = this.x + this.width/2;
-    this.yCenter = this.y + this.height/2;
+    this.xCenter = this.x + this.width / 2;
+    this.yCenter = this.y + this.height / 2;
   }
 
   //Adds glow to structure
   addGlow() {
-    ctx.shadowBlur = 2*unit;
+    ctx.shadowBlur = 2 * unit;
     ctx.shadowColor = this.color;
   }
 }
 
 //Constructs Player Class
 class Player extends Structure {
-  constructor (width, height, x, y, direction, color) {
+  constructor(width, height, x, y, direction, color) {
     super(["dynamic", "consumer"], width, height, x, y, color);
     this.direction = direction;
     this.mass = 1;
     this.airTime = 0;
     this.jumpInit = false;
     this.isJumping = false;
-    this.jumpPowerInit = 1.75*unit;
+    this.jumpPowerInit = 1.75 * unit;
     this.jumpPower = this.jumpPowerInit;
-    this.jumpPowerMax = 2.5*unit;
+    this.jumpPowerMax = 2.5 * unit;
     this.jumpChargeTimeInit = 5;
     this.jumpChargeTime = this.jumpChargeTimeInit;
     this.speed = 0;
-    this.speedCap = this.jumpPowerInit/2;
-    this.acceleration = this.jumpPowerInit/5;
+    this.speedCap = this.jumpPowerInit / 2;
+    this.acceleration = this.jumpPowerInit / 5;
     this.rotation = 0;
     this.rotationI = 0;
     this.rotationN = 0;
     this.score = {
       value: 0,
       valueCurr: 0,
-      valueSpeed: (this.value/this.valueCurr)/30,
+      valueSpeed: (this.value / this.valueCurr) / 30,
       isNew: null,
     };
   }
@@ -91,20 +93,20 @@ class Player extends Structure {
 
 //Constructs Food Class
 class Food extends Structure {
-  constructor () {
-    let width = Math.random() * 1*unit + 1.5*unit;
+  constructor() {
+    let width = Math.random() * 1 * unit + 1.5 * unit;
     let height = width;
     let colors = ['#0f8', '#f88', '#ff8', '#8ff', '#f8f', '#fff'];
-    super (
-      "fixed", 
-      width, 
-      height, 
-      Math.random() * (c.width - width), 
-      (Math.random() * 1/3*c.height - height) + 1/3*c.height, 
-      colors[Math.floor(Math.random() * colors.length)], 
+    super(
+      "fixed",
+      width,
+      height,
+      Math.random() * (c.width - width),
+      (Math.random() * 1 / 3 * c.height - height) + 1 / 3 * c.height,
+      colors[Math.floor(Math.random() * colors.length)],
     )
-    this.score = Math.ceil(this.width/2);
-    this.colors = colors;  
+    this.score = Math.ceil(this.width / unit);
+    this.colors = colors;
     this.exists = true;
     this.spawnTimeInit = 45;
     this.spawnTime = this.spawnTimeInit;
@@ -115,21 +117,22 @@ class Food extends Structure {
       draw(x, y, radius) {
         ctx.beginPath();
         ctx.strokeStyle = this.color;
-        ctx.shadowBlur = 2.5*unit;
-        ctx.arc(x, y, radius*0.1*unit, 0, 2*Math.PI);
+        ctx.shadowBlur = 2.5 * unit;
+        ctx.arc(x, y, radius * 0.1 * unit, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.closePath();
       }
     }
   }
 
+  //Randomizes the properties of the food particle (after it has been eaten)
   updateProperties() {
-    this.width = Math.random() * 1*unit + 1.5*unit;
+    this.width = Math.random() * 1 * unit + 1.5 * unit;
     this.height = this.width;
     this.x = Math.random() * (c.width - this.width);
-    this.y = (Math.random() * (1/3) * c.height - this.height) + (1/3) * c.height; 
+    this.y = (Math.random() * (1 / 3) * c.height - this.height) + (1 / 3) * c.height;
     this.color = this.colors[Math.floor(Math.random() * this.colors.length)];
-    this.score = Math.ceil(this.width/2);
+    this.score = Math.ceil(this.width / unit);
   }
 }
 
@@ -140,11 +143,11 @@ class Food extends Structure {
 
 //Creates game components
 const game = {
-  friction: new Physics(.02*unit),
-  gravity: new Physics(.25*unit),
-  floor: new Structure(["fixed"], c.width, c.height*1/3, 0, c.height*2/3, "#333"),
-  bg: new Structure(["fixed"], c.width, c.height*2/3 + 1*unit, 0, 0, "#222"),
-  player: new Player(5*unit, 5*unit, 10*unit, 8*unit, 1, "#0ff"),
+  friction: new Physics(.02 * unit),
+  gravity: new Physics(.25 * unit),
+  floor: new Structure(["fixed"], c.width, c.height * 1 / 3, 0, c.height * 2 / 3, "#333"),
+  bg: new Structure(["fixed"], c.width, c.height * 2 / 3 + 1 * unit, 0, 0, "#222"),
+  player: new Player(5 * unit, 5 * unit, 10 * unit, 8 * unit, 1, "#0ff"),
   food: [],
 }
 
@@ -227,16 +230,19 @@ const applyJump = () => {
         comp.speed = comp.speedCap;
       }
       if (comp.jumpChargeTime > 0) {
-        comp.jumpPower += (comp.jumpPowerMax - comp.jumpPowerInit)/5;
+        comp.jumpPower += (comp.jumpPowerMax - comp.jumpPowerInit) / 5;
         if (comp.jumpPower > comp.jumpPowerMax) {
           comp.jumpPower = comp.jumpPowerMax;
         }
       }
+
+      //Declares simulation variables
       let simPow = comp.jumpPower;
       let simY = 0;
       let simG = gravity.magnitude;
       let simAir = -1;
 
+      //Runs a simulation to predict the airTime of the box before it hits the ground
       do {
         simAir++;
         simY -= simPow;
@@ -244,6 +250,7 @@ const applyJump = () => {
       } while (simY < 0);
       player.rotationI = simAir;
     }
+
     if (comp.isJumping) {
       comp.y -= comp.jumpPower;
       if (comp.y <= floor.y - comp.height && comp.jumpPower > (gravity.magnitude * comp.airTime)) {
@@ -263,7 +270,7 @@ const applyJump = () => {
         comp.jumpChargeTime = 0;
       }
       if (comp.jumpChargeTime === 0) {
-        comp.jumpPower -= .05*unit;
+        comp.jumpPower -= .05 * unit;
         if (comp.jumpPower < comp.jumpPowerInit) {
           comp.jumpPower = comp.jumpPowerInit;
         }
@@ -286,7 +293,7 @@ const applyShift = () => {
 const applyWallHitBox = () => {
   for (let comp in game) {
     comp = game[comp];
-    let { type, x, direction, width } = comp; 
+    let { type, x, direction, width } = comp;
     if (type) {
       if (type.includes('dynamic')) {
         if (x < 0 && direction === -1) {
@@ -322,7 +329,7 @@ const drawPlayer = () => {
 
   //Calculates the degree by which player should be rotated
   if (player.y < floor.y - player.height) {
-    player.rotation += 90/player.rotationI;
+    player.rotation += 90 / player.rotationI;
   } else {
     player.rotation = 90 * player.rotationN;
   }
@@ -344,10 +351,10 @@ const applyFoodEdibility = () => {
       comp = game[comp];
       if (comp.type) {
         if (comp.type.includes("consumer")) {
-          if (comp.x <= food.x + food.width && 
-              comp.x + comp.width >= food.x &&
-              comp.y <= food.y + food.height &&
-              comp.y + comp.height >= food.y) {
+          if (comp.x <= food.x + food.width &&
+            comp.x + comp.width >= food.x &&
+            comp.y <= food.y + food.height &&
+            comp.y + comp.height >= food.y) {
             food.exists = false;
             comp.score.value += food.score;
             food.score = 0;
@@ -362,9 +369,9 @@ const applyFoodEdibility = () => {
 //Draws the player's score
 const drawScore = () => {
   const score = game.player.score;
-  score.valueSpeed = (score.value - score.valueCurr)/30;
-  if (score.valueSpeed < game.food[0].score/30) {
-    score.valueSpeed = game.food[0].score/30;
+  score.valueSpeed = (score.value - score.valueCurr) / 30;
+  if (score.valueSpeed < game.food[0].score / 30) {
+    score.valueSpeed = game.food[0].score / 30;
   }
   score.valueCurr += score.valueSpeed;
   if (score.valueCurr > score.value) {
@@ -372,9 +379,9 @@ const drawScore = () => {
   }
   ctx.beginPath();
   ctx.fillStyle = player.color;
-  ctx.font = `normal 300 ${5*unit}px Arial`;
+  ctx.font = `normal 300 ${5 * unit}px Arial`;
   ctx.textAlign = "center";
-  ctx.fillText(`${parseInt(score.valueCurr)} Points`, c.width/2, c.width/13.33333);
+  ctx.fillText(`${parseInt(score.valueCurr)} Points`, c.width / 2, c.width / 13.33333);
   ctx.closePath();
 }
 
@@ -391,7 +398,7 @@ const drawFood = () => {
     if (food.exists) {
       food.addGlow();
       food.draw();
-    } 
+    }
     if (food.exists === false) {
       food.dreg.counter++;
       if (food.spawnTime > 0) {
@@ -408,12 +415,12 @@ const drawFood = () => {
 /* ===============================
     Game Functions Implementation 
    =============================== */
-   
+
 //Calls all game functions every frame.
 const animate = () => {
 
   window.requestAnimationFrame(animate);
-  
+
   clearCanvas();
   calcAirTime();
   applyGravity();
@@ -439,30 +446,18 @@ window.addEventListener("keydown", function (e) {
   if (e.key == "w" || e.key == "ArrowUp" || e.key == "W") {
     player.jumpInit = true;
   }
-});
-window.addEventListener("keydown", function (e) {
+
   if (e.key == "a" || e.key == "ArrowLeft" || e.key == "A") {
     player.direction = -1;
   }
-});
-window.addEventListener("keydown", function (e) {
+
   if (e.key == "s" || e.key == "ArrowDown" || e.key == "S") {
     player.speed = 0;
     player.jumpPower = player.jumpPowerInit;
     player.jumpChargeTime = 0;
   }
-})
-window.addEventListener("keydown", function (e) {
+
   if (e.key == "d" || e.key == "ArrowRight" || e.key == "D") {
     player.direction = 1;
   }
 });
-
-/* ===============================
-              Problems
-   =============================== */
-
-  //Food grows and shrinks
-  //Ground needs to glow when player falls
-  //Needs Improved Documentation
-  //Needs graph explaining player status
